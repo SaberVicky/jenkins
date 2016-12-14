d91276d7bc566d09b0e857f3d318108b3fd6c7ec
泡泡龙 <songlongwind@126.com>
2016-12-14 17:03:13 +0800

    1

---
+++ jenkins/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  jenkins
+//
+//  Created by songlong on 2016/12/14.
+//  Copyright © 2016年 com.Saber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ jenkins/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  jenkins
+//
+//  Created by songlong on 2016/12/14.
+//  Copyright © 2016年 com.Saber. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
